163ae2142858cc9fe3a4a629ddedb508b56d6161
Masteriplab <Masteriplab@gmail.com>
2025-04-21 09:45:31 +0500

    Psychra.py

--- Psychra.py
+++ Psychra.py
@@ -11,7 +11,7 @@ YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
 
 # Streamlit App Title
-st.title("YouTube Viral Topics Tool")
+st.title("YouTube Viral Topic Tool")
 
 # Input Fields
 days = st.number_input("Enter Days to Search (1-30):", min_value=1, max_value=30, value=5)
